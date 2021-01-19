ca35aaf246bfd6852a8a35b59df1a6eebd798edc
Alisultan Abdullah <47978568+AlisultanAbdullayev@users.noreply.github.com>
2021-01-19 21:44:35 +0300

    Commit

---
+++ Slot Machine/Views/InfoView.swift
@@ -0,0 +1,72 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by Alisultan Abdullah on 11.12.2020.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+                Section(header: Text("About the application")) {
+                    FormRowView(firstItem: "Application", secondItem: "Slot Machine")
+                    FormRowView(firstItem: "Platforms", secondItem: "iPhone, iPad, Mac")
+                    FormRowView(firstItem: "Developer", secondItem: "Alisultan Abdullah")
+                    FormRowView(firstItem: "Designer", secondItem: "Robert Petras")
+                    FormRowView(firstItem: "Music", secondItem: "Dan Lebowitz")
+                    FormRowView(firstItem: "Website", secondItem: "http://h70113i0.beget.tech")
+                    FormRowView(firstItem: "Version", secondItem: "1.0.1")
+                    FormRowView(firstItem: "Copyright", secondItem: "© All rights reserved.")
+                }
+            }
+            .font(.system(.body, design: .rounded))
+        }
+        .padding(.top, 40)
+        .overlay(
+            Button(action: {
+                audioPlayer?.stop()
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "xmark.circle")
+                    .font(.title)
+            }
+            .padding(.top, 30)
+            .padding(.trailing, 20)
+            .accentColor(Color.secondary)
+            , alignment: .topTrailing
+        )
+        .onAppear(perform: {
+            playSound(sound: "background-music", type: "mp3")
+        })
+    }
+}
+
+struct FormRowView: View {
+    var firstItem: String
+    var secondItem: String
+    
+    var body: some View {
+        HStack {
+            Text(firstItem).foregroundColor(Color.gray)
+            Spacer()
+            Text(secondItem)
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+        
+    }
+}
+
